d4aea8c7b52419e8c00075a5b9fe6525b41bf640
Paul Gardner-Stephen <paul@servalproject.org>
2019-06-24 20:27:22 +0200

    add scripts for automated building and flashing of MAX10 bitstream

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATH=${PATH}:/opt/Quartus/18.1/quartus/bin
+
+quartus_map --read_settings_files=on --write_settings_files=off mega65-r2-max10 -c mega65-r2-max10 && \
+quartus_fit --read_settings_files=on --write_settings_files=off mega65-r2-max10 -c mega65-r2-max10 && \
+quartus_asm --read_settings_files=off --write_settings_files=off mega65-r2-max10 -c mega65-r2-max10
---
+++ program.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PATH=${PATH}:/opt/Quartus/18.1/quartus/bin
+
+quartus_pgm -m jtag -o "p;output_files/mega65-r2-max10.sof"
