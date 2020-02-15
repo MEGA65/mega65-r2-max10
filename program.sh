13a94057aa6cd26badcf4300211c1befc9d7e529
Paul Gardner-Stephen <paul@servalproject.org>
2020-02-15 15:03:12 +1030

    add commands to force reset jtagd etc before programming.

--- program.sh
+++ program.sh
@@ -2,4 +2,8 @@
 
 PATH=${PATH}:/opt/Quartus/18.1/quartus/bin
 
+sudo pkill jtagd
+sudo /opt/Quartus/18.1/quartus/bin/jtagd
+sudo /opt/Quartus/18.1/quartus/bin/jtagconfig
+
 quartus_pgm -m jtag -o "p;output_files/mega65-r2-max10.sof"
