b383441cbd890b2eb64417865dd75a06dd8c17aa
Paul Gardner-Stephen <paul@m-e-g-a.org>
2022-11-20 17:16:15 +1030

    update from latest build

--- version.vhdl
+++ version.vhdl
@@ -5,8 +5,8 @@ use ieee.numeric_std.all;
 
 package version is
 
-  constant gitcommit : string := "diy-keyboard,80,20221120.16,07d6587~";
-  constant fpga_commit : unsigned(31 downto 0) := x"07d6587e";
+  constant gitcommit : string := "diy-keyboard,82,20221120.16,a18f0c5~";
+  constant fpga_commit : unsigned(31 downto 0) := x"a18f0c53";
   constant fpga_datestamp : unsigned(15 downto 0) := to_unsigned(1056,16);
 
 end version;
