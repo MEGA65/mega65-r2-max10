1034636762198afdcf12066cf83eb622ebebbe69
Paul Gardner-Stephen <paul@m-e-g-a.org>
2022-12-09 18:35:36 +1030

    add script for compiling libpng12 if missing

---
+++ libpng12.sh
@@ -0,0 +1,11 @@
+sudo apt install build-essential zlib1g-dev
+mkdir tmp-src
+cd tmp-src
+wget https://ppa.launchpadcontent.net/linuxuprising/libpng12/ubuntu/pool/main/libp/libpng/libpng_1.2.54.orig.tar.xz
+tar Jxfv libpng_1.2.54.orig.tar.xz
+cd libpng-1.2.54
+./configure
+make
+sudo make install
+sudo ln -s /usr/local/lib/libpng12.so.0.54.0 /usr/lib/libpng12.so
+sudo ln -s /usr/local/lib/libpng12.so.0.54.0 /usr/lib/libpng12.so.0
